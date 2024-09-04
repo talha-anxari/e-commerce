af9a51d819f38f6d796e1b6f387019c33afb3e37
talha-anxari <talhaanxaritn@outlook.com>
2024-09-04 11:20:38 +0500

    update

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,12 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux';
-import store from './redux/Store.jsx';
+import Store from './redux/Store.jsx';
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider store={store}>
+    <Provider store={Store}>
     <App />
     </Provider>
   </StrictMode>,
