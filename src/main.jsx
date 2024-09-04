7bee9626deb90dcc7090fcf6ba40a856c60f2cc6
talha-anxari <talhaanxaritn@outlook.com>
2024-09-04 11:23:34 +0500

    update

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux';
-import Store from './redux/Store.jsx';
+import Store from './redux/Store';
 
 
 createRoot(document.getElementById('root')).render(
